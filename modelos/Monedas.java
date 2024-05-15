2db9be44f0f30ab6f90e222034fe7618332e3eae
Daniel Cano Suarez <dcanosu@eafit.edu.co>
2024-05-14 20:23:29 -0500

    modificacion menu de opciones

--- modelos/Monedas.java
+++ modelos/Monedas.java
@@ -7,21 +7,28 @@ import java.util.Scanner;
 import servicios.GestorConversor;
 
 public class Monedas {
+    private Scanner scanner;
     private GestorConversor gestorConversor;
 
     public Monedas(GestorConversor gestorConversor) {
         this.gestorConversor = gestorConversor;
+        this.scanner = new Scanner(System.in);
     }
 
     public void convertirMoneda(String descripcionCurrency, String fromCurrency, String descripcionToCurrency, String toCurrency, String descripcion) {
         try {
             System.out.println("Por favor ingrese la cantidad de " + descripcionCurrency + "["+fromCurrency+"]" + " que desea convertir a " + descripcionToCurrency+"["+toCurrency+"]" + ": ");
-            Scanner scanner = new Scanner(System.in);
             double cantidad = scanner.nextDouble();
             double resultado = gestorConversor.convertirMoneda(fromCurrency, toCurrency, cantidad);
             mostrarResultado(descripcion, cantidad, toCurrency, fromCurrency, resultado);
         } catch (IOException e) {
             System.out.println("Error al realizar la conversión: " + e.getMessage());
+        }
+    }
+
+    public void cerrarScanner() {
+        if (scanner != null) {
+            scanner.close();
         }
     }
 
@@ -44,25 +51,25 @@ public class Monedas {
     }
     
     public void convertirDolarAPesoArgentino() {
-        convertirMoneda("dolares","USD","pesos argentinos", "ARS", "Convertir de dólar a peso argentino:");
+        convertirMoneda("dolares","USD","pesos argentinos", "ARS", "Convertir de dólar a peso argentino");
     }
     public void convertirPesoArgentinoADolar() {
-        convertirMoneda("pesos argentinos","ARS", "dolares","USD", "Convertir de peso argentino a dólar:");
+        convertirMoneda("pesos argentinos","ARS", "dolares","USD", "Convertir de peso argentino a dólar");
     }
 
     public void convertirDolarARealBrasileno() {
-        convertirMoneda("dolares","USD", "reales brasileños","BRL", "Convertir de dólar a reales brasileño:");
+        convertirMoneda("dolares","USD", "reales brasileños","BRL", "Convertir de dólar a reales brasileño");
     }
 
     public void convertirRealBrasilenoADolar() {
-        convertirMoneda("reales brasileños","BRL","dolares", "USD", "Convertir de real brasileño a dólar:");
+        convertirMoneda("reales brasileños","BRL","dolares", "USD", "Convertir de real brasileño a dólar");
     }
 
     public void convertirDolarAPesoColombiano() {
-        convertirMoneda("dolares","USD", "pesos colombianos","COP", "Convertir de dólar a peso colombiano:");
+        convertirMoneda("dolares","USD", "pesos colombianos","COP", "Convertir de dólar a peso colombiano");
     }
 
     public void convertirPesoColombianoADolar() {
-        convertirMoneda("pesos colombianos","COP","dolares", "USD", "Convertir de peso colombiano a dólar:");
+        convertirMoneda("pesos colombianos","COP","dolares", "USD", "Convertir de peso colombiano a dólar");
     }
 }
--- servicios/Opciones.java
+++ servicios/Opciones.java
@@ -15,64 +15,70 @@ public class Opciones extends Menus{
 
     public void mostrarMenu() {
         Scanner scanner = new Scanner(System.in);
-
+    
         while (true) {
             menuInicial();
             int opcionIngresoMenu = scanner.nextInt();
             switch (opcionIngresoMenu) {
                 case 1:
-                    menuDivisas();
-                    int opcion = scanner.nextInt();
-                    switch (opcion) {
-                        case 1:
-                            System.out.println(asteriscos);
-                            System.out.println("Convertidor de dólar a peso argentino");
-                            System.out.println(asteriscos);
-                            Monedas monedas1 = new Monedas(gestorConversor);
-                            monedas1.convertirDolarAPesoArgentino();
+                    while (true) {
+                        menuDivisas();
+                        int opcion = scanner.nextInt();
+                        switch (opcion) {
+                            case 1:
+                                System.out.println(asteriscos);
+                                System.out.println("Convertidor de dólar a peso argentino");
+                                System.out.println(asteriscos);
+                                Monedas monedas1 = new Monedas(gestorConversor);
+                                monedas1.convertirDolarAPesoArgentino();
+                                break;
+                            case 2:
+                                System.out.println(asteriscos);
+                                System.out.println("Convertidor de peso argentino a dólar");
+                                System.out.println(asteriscos);
+                                Monedas monedas2 = new Monedas(gestorConversor);
+                                monedas2.convertirPesoArgentinoADolar();
+                                break;
+                            case 3:
+                                System.out.println(asteriscos);
+                                System.out.println("Convertidor de dólar a real brasileño");
+                                System.out.println(asteriscos);
+                                Monedas monedas3 = new Monedas(gestorConversor);
+                                monedas3.convertirDolarARealBrasileno();
+                                break;
+                            case 4:
+                                System.out.println(asteriscos);
+                                System.out.println("Convertidor de real brasileño a dólar");
+                                System.out.println(asteriscos);
+                                Monedas monedas4 = new Monedas(gestorConversor);
+                                monedas4.convertirRealBrasilenoADolar();
+                                break;
+                            case 5:
+                                System.out.println(asteriscos);
+                                System.out.println("Convertidor de dólar a peso colombiano");
+                                System.out.println(asteriscos);
+                                Monedas monedas5 = new Monedas(gestorConversor);
+                                monedas5.convertirDolarAPesoColombiano();
+                                break;
+                            case 6:
+                                System.out.println(asteriscos);
+                                System.out.println("Convertidor de peso colombiano a dólar");
+                                System.out.println(asteriscos);
+                                Monedas monedas6 = new Monedas(gestorConversor);
+                                monedas6.convertirPesoColombianoADolar();
+                                break;
+                            case 7:
+                                saliendoDelPrograma();
+                                scanner.close();
+                                return;
+                            default:
+                                opcionIncorrecta();
+                                break;
+                        }
+                        // Si el usuario eligió una opción válida, salimos del bucle interno
+                        if (opcion >= 1 && opcion <= 7) {
                             break;
-                        case 2:
-                            System.out.println(asteriscos);
-                            System.out.println("Convertidor de peso argentino a dólar");
-                            System.out.println(asteriscos);
-                            Monedas monedas2 = new Monedas(gestorConversor);
-                            monedas2.convertirPesoArgentinoADolar();
-                            break;
-                        case 3:
-                            System.out.println(asteriscos);
-                            System.out.println("Convertidor de dólar a real brasileño");
-                            System.out.println(asteriscos);
-                            Monedas monedas3 = new Monedas(gestorConversor);
-                            monedas3.convertirDolarARealBrasileno();
-                            break;
-                        case 4:
-                            System.out.println(asteriscos);
-                            System.out.println("Convertidor de real brasileño a dólar");
-                            System.out.println(asteriscos);
-                            Monedas monedas4 = new Monedas(gestorConversor);
-                            monedas4.convertirRealBrasilenoADolar();
-                            break;
-                        case 5:
-                            System.out.println(asteriscos);
-                            System.out.println("Convertidor de dólar a peso colombiano");
-                            System.out.println(asteriscos);
-                            Monedas monedas5 = new Monedas(gestorConversor);
-                            monedas5.convertirDolarAPesoColombiano();
-                            break;
-                        case 6:
-                            System.out.println(asteriscos);
-                            System.out.println("Convertidor de peso colombiano a dólar");
-                            System.out.println(asteriscos);
-                            Monedas monedas6 = new Monedas(gestorConversor);
-                            monedas6.convertirPesoColombianoADolar();
-                            break;
-                        case 7:
-                            saliendoDelPrograma();
-                            scanner.close();
-                            return;
-                        default:
-                            opcionIncorrecta();
-                            break;
+                        }
                     }
                     break;
                 case 2:
